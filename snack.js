f53d8c518cf9bd0c784b39fc986a1e3533f6f676
Marcucci-Francesco <fmarcucci47@gmail.com>
2025-06-15 14:21:39 +0200

    [feat] add snack 1-describe

--- snack.js
+++ snack.js
@@ -16,8 +16,43 @@ function average(numeri) {
   return numeri.reduce((acc, val) => acc + val, 0) / numeri.length;
 }
 
+
+function createSlug(titolo) {
+  return titolo.toLowerCase().replaceAll(" ", "-");
+}
+
+function isPalindrome(parola) {
+  const parolaInversa = parola.trim().split('').reverse().join('');
+  return parola.trim() === parolaInversa;
+}
+
+function createSlug(titolo) {
+  if (!titolo) {
+    throw new Error("Il titolo non può essere vuoto.");
+  }
+  return titolo.toLowerCase().replaceAll(" ", "-");
+}
+
+
+function findPostsById(posts, id) {
+  if (isNaN(id)) {
+    throw new Error("L'ID deve essere un numero.");
+  }
+  posts.forEach(p => {
+    if (p.id === undefined || p.title === undefined || p.slug === undefined) {
+      throw new Error("Ogni post deve avere un ID numerico.");
+    }
+  });
+  return posts.find(post => post.id === id) || null;
+}
+
+
 module.exports = {
   getInitials,
   createSlug,
-  average
+  average,
+  createSlug,
+  isPalindrome,
+  createSlug,
+  findPostsById
 }
--- snacks.test.js
+++ snacks.test.js
@@ -1,70 +1,88 @@
 const { } = require("./snack.js")
 
 
-// Snack 1
+describe('Manipolazione di stringhe', () => {
 
-const { getInitials } = require("./snack.js");
-test('La funzione getInitials restituisce le iniziali di un nome completo.', () => {
-  expect(getInitials("Mario Rossi")).toBe("MR");
-})
+  // Snack 1
 
-
-
-// Snack 2
-
-const { createSlug } = require("./snack.js");
-test('La funzione createSlug restituisce una stringa in lowercase.', () => {
-  expect(createSlug("Ciao Mondo")).toBe("ciao mondo");
-})
+  const { getInitials } = require("./snack.js");
+  test('La funzione getInitials restituisce le iniziali di un nome completo.', () => {
+    expect(getInitials("Mario Rossi")).toBe("MR");
+  })
 
 
+  // Snack 5
+  const { isPalindrome } = require("./snack.js");
 
-// Snack 3
-const { average } = require("./snack.js");
-
-test('La funzione average calcola la media aritmetica di un array di numeri.', () => {
-  expect(average([20, 10])).toBe(15);
-})
-
-
-
-// Snack 4
-
-test('La funzione createSlug sostituisce gli spazi con -.', () => {
+  test('La funzione isPalindrome verifica se una stringa è un palindromo.', () => {
+    expect(isPalindrome("anna")).toBeTruthy();
+  })
 
 })
 
 
+describe('Operazioni su Array', () => {
 
 
-// Snack 5
+  // Snack 3
+  const { average } = require("./snack.js");
 
-test('La funzione isPalindrome verifica se una stringa è un palindromo.', () => {
+  test('La funzione average calcola la media aritmetica di un array di numeri.', () => {
+    expect(average([20, 10])).toBe(15);
+  })
+
+
+  // Snack 7
+
+  const posts = [
+    { id: 1, title: "Post 1", slug: "post-1" },
+    { id: 2, title: "Post 2", slug: "post-2" },
+    { id: 3, title: "Post 3", slug: "post-3" }
+  ];
+
+  const { findPostsById } = require("./snack.js");
+
+  test('La funzione findPostById restituisce il post corretto dato l’array di post e l’id', () => {
+    expect(findPostsById(posts, 2)).toEqual({ id: 2, title: "Post 2", slug: "post-2" });
+    expect(() => findPostsById(posts, 4)).toThrow("Post non trovato");
+
+  })
+
+
+})
+
+
+describe('Generazione di slag', () => {
+
+
+  // Snack 2
+
+  const { createSlug } = require("./snack.js");
+  test('La funzione createSlug restituisce una stringa in lowercase.', () => {
+    expect(createSlug("Ciao Mondo")).toBe("ciao mondo");
+  })
+
+
+  // Snack 4
+
+  const { createSlug } = require("./snack.js");
+
+  test('La funzione createSlug sostituisce gli spazi con -.', () => {
+    expect(createSlug("Ciao Mondo")).toBe("ciao-mondo");
+  })
+
+
+  // Snack 6
+
+  const { createSlug } = require("./snack.js");
+
+  test('La funzione createSlug lancia un errore se il titolo è vuoto o non valido.', () => {
+    expect(() => createSlug("")).toThrow("Il titolo non può essere vuoto.");
+  })
 
 })
 
 
 
 
-// Snack 6
 
-test('La funzione createSlug lancia un errore se il titolo è vuoto o non valido.', () => {
-
-})
-
-
-
-
-// Snack 7
-
-test('La funzione findPostById restituisce il post corretto dato l’array di post e l’id', () => {
-
-})
-
-
-
-// Snack Describe()
-
-test('', () => {
-
-})
